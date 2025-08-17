2be9b7dab66d94587d6d42647e2c53e7f54c56f5
Mahesh <maheshreddymallidi01@gmail.com>
2025-08-17 23:47:58 +0530

    Latest

--- Practise/src/com/practise/Recursion/RecursionPractise4.java
+++ Practise/src/com/practise/Recursion/RecursionPractise4.java
@@ -10,6 +10,49 @@ public class RecursionPractise4 {
 		int n = arr.length;
 		List<Integer> mmr = new ArrayList<>();
 		printSS(0,mmr,arr,n);
+		
+		int sum = 3;
+		printSSWhenSum(0,mmr,0,sum,arr,n);
+		printSSWhenSum2(0,mmr,0,sum,arr,n);
+	}
+	
+	private static boolean printSSWhenSum2(int ind,List<Integer> mmr,int s,int sum, int[] arr,int n) {
+		if(ind==n) {
+			if(s==sum) {
+				for(int it:mmr) {
+					System.out.print(it+" ");
+				}
+				System.out.println();
+				return true;
+			}else {
+				return false;
+			}
+		}
+		s+=arr[ind];
+		mmr.add(arr[ind]);
+		if(printSSWhenSum2(ind+1,mmr,s,sum,arr,n)) return true;
+		s-=arr[ind];
+		mmr.remove(mmr.size()-1);
+		if(printSSWhenSum2(ind+1,mmr,s,sum,arr,n)) return true;
+		return false;
+	}
+	
+	private static void printSSWhenSum(int ind,List<Integer> mmr,int s,int sum, int[] arr,int n) {
+		if(ind==n) {
+			if(s==sum) {
+				for(int it:mmr) {
+					System.out.print(it+" ");
+				}
+			}
+			System.out.println();
+			return;
+		}
+		s+=arr[ind];
+		mmr.add(arr[ind]);
+		printSSWhenSum(ind+1,mmr,s,sum,arr,n);
+		s-=arr[ind];
+		mmr.remove(mmr.size()-1);
+		printSSWhenSum(ind+1,mmr,s,sum,arr,n);
 	}
 	
 	public static void printSS(int ind,List<Integer> mmr,int[] arr,int n) {

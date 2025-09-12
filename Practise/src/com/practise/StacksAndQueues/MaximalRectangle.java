c296fd54c0979bbc53f5143fedbb57e49347247b
Mahesh <maheshreddymallidi01@gmail.com>
2025-09-12 20:56:09 +0530

    Maximal Rectangle

--- Practise/src/com/practise/StacksAndQueues/MaximalRectangle.java
+++ Practise/src/com/practise/StacksAndQueues/MaximalRectangle.java
@@ -33,14 +33,19 @@ public class MaximalRectangle {
         Deque<Integer> stack = new ArrayDeque<>();
         int maxArea = 0;
 
-        for (int i = 0; i <= n; i++) {
-            int h = (i == n ? 0 : heights[i]);
-            while (!stack.isEmpty() && h < heights[stack.peek()]) {
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()]>heights[i]) {
                 int height = heights[stack.pop()];
                 int width = stack.isEmpty() ? i : i - stack.peek() - 1;
                 maxArea = Math.max(maxArea, height * width);
             }
             stack.push(i);
+        }
+        
+        while(!stack.isEmpty()) {
+        	 int height = heights[stack.pop()];
+             int width = stack.isEmpty() ? n : n - stack.peek() - 1;
+             maxArea = Math.max(maxArea, height * width);
         }
         return maxArea;
     }
